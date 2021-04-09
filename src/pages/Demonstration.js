685c7acf6e3c6fa9f2da89d511c517d6b237aab1
amandahp <amandahoffmann96@gmail.com>
2021-04-09 14:42:31 -0300

    feat: added helmet

--- src/pages/Demonstration.js
+++ src/pages/Demonstration.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import { ThemeProvider } from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faSearch} from '@fortawesome/free-solid-svg-icons';
+import { Helmet } from 'react-helmet';
 
 
 import productOne from '../theme/productOne';
@@ -37,66 +38,77 @@ function Demonstration() {
 
 
   return (
-    <ThemeProvider theme={currentTheme}>
-      <Styled.Wrapper>
-        <Styled.Header>
-          <div className="container">
-            {currentTheme === productOne && (
-                <Trello />
-            )}{currentTheme === productTwo && (
-                <Ubuntu />
-            )}{currentTheme === productThree && (
-                <Slides />
-            )}
-            <form>
-              {products.map((product, index) => {
-                return(
-                  <label for={`product-${index+1}`}>
-                  <input
-                    key={index+2}                
-                    type="radio"
-                    id={`product-${index+1}`}
-                    name="theme"
-                    value={`product-${index+1}`}
-                    checked={currentTheme === product}
-                    onChange={() => setCurrentTheme(product)}
-                  />
-                  Produto {index+1}
-                </label>
-                )
-              } )}
+    <>
+    <Helmet>
+      {currentTheme === productOne && (
+            <title>Produto 1</title>
+        )}{currentTheme === productTwo && (
+            <title>Produto 2</title>
+        )}{currentTheme === productThree && (
+          <title>Produto 3</title>
+      )}
+    </Helmet>
+      <ThemeProvider theme={currentTheme}>
+        <Styled.Wrapper>
+          <Styled.Header>
+            <div className="container">
+              {currentTheme === productOne && (
+                  <Trello />
+              )}{currentTheme === productTwo && (
+                  <Ubuntu />
+              )}{currentTheme === productThree && (
+                  <Slides />
+              )}
+              <form>
+                {products.map((product, index) => {
+                  return(
+                    <label for={`product-${index+1}`}>
+                    <input
+                      key={index+2}                
+                      type="radio"
+                      id={`product-${index+1}`}
+                      name="theme"
+                      value={`product-${index+1}`}
+                      checked={currentTheme === product}
+                      onChange={() => setCurrentTheme(product)}
+                    />
+                    Produto {index+1}
+                  </label>
+                  )
+                } )}
+              </form>
+            </div>
+          </Styled.Header>
+          <Styled.Title>
+            <div className="container">
+              <p>Modelo de Design System</p>
+            </div>
+            <form action='/' method='post'>
+              <input type='text' name='pesquisa' placeholder='O que você procura?' />
+              <button type='submit'>{iconSearch}</button>
             </form>
-          </div>
-        </Styled.Header>
-        <Styled.Title>
-          <div className="container">
-            <p>Modelo de Design System</p>
-          </div>
-          <form action='/' method='post'>
-            <input type='text' name='pesquisa' placeholder='O que você procura?' />
-            <button type='submit'>{iconSearch}</button>
-          </form>
-        </Styled.Title>
-        <Styled.BannerWrapper image={urlBanner}/>
-          <Styled.Information>
-            <div className="container">
-              <p>
-                Lorem <span>Ipsum</span> Dolor <span>sit amet</span>
-              </p>
-            </div>
-          </Styled.Information>
-          <Styled.Content>
-            <div className="container">
-            <h2>Lorem ipsum</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            <button><a href='https://medium.com/' rel="noopener noreferrer" target='_blank'>Lorem Ipsum</a></button>
-            </div>
-          </Styled.Content>
-        <Styled.Footer>
-          <h2>Design System</h2>
-        </Styled.Footer>
-    </Styled.Wrapper>
-    </ThemeProvider>
+          </Styled.Title>
+          <Styled.BannerWrapper image={urlBanner}/>
+            <Styled.Information>
+              <div className="container">
+                <p>
+                  Lorem <span>Ipsum</span> Dolor <span>sit amet</span>
+                </p>
+              </div>
+            </Styled.Information>
+            <Styled.Content>
+              <div className="container">
+              <h2>Lorem ipsum</h2>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              <button><a href='https://medium.com/' rel="noopener noreferrer" target='_blank'>Lorem Ipsum</a></button>
+              </div>
+            </Styled.Content>
+          <Styled.Footer>
+            <h2>Design System</h2>
+          </Styled.Footer>
+      </Styled.Wrapper>
+      </ThemeProvider>
+    </>
   );
 }
 
